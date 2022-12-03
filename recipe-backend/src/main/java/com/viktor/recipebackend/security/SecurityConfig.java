bf3c836e6a9e296f1fc93c4ce873c5df0c07ad23
Viktor <tsvetovich@gmail.com>
2022-12-03 22:05:23 +0300

    Добавлен файл конфигурации с авторизацией запросов

---
+++ recipe-backend/src/main/java/com/viktor/recipebackend/security/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.viktor.recipebackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain makeSecurityRequests(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("recipe/**").permitAll()
+                .requestMatchers("user/**").permitAll());
+        return httpSecurity.build();
+    }
+}
