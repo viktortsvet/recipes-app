1e2b074bd17270738d9a42fa2b4fb9f4d26a7f6c
Viktor <tsvetovich@gmail.com>
2022-12-03 19:46:07 +0300

    Развёрнут фронтэнд и бэкенд

---
+++ recipe-backend/src/test/java/com/viktor/recipebackend/RecipeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viktor.recipebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
