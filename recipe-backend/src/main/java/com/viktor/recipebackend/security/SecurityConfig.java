d87eca5aef1c1bca96b6780e70e7e5cdf7aef3bf
Viktor <tsvetovich@gmail.com>
2022-12-03 22:41:51 +0300

    Немного файл конфигурации с авторизацией запросов

--- recipe-backend/src/main/java/com/viktor/recipebackend/security/SecurityConfig.java
+++ recipe-backend/src/main/java/com/viktor/recipebackend/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,7 +12,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain makeSecurityRequests(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(authorize -> authorize
+        httpSecurity.cors().disable().httpBasic().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("recipe/**").permitAll()
                 .requestMatchers("user/**").permitAll());
         return httpSecurity.build();
